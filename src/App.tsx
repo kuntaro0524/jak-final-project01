105bcca199de28d8ba38165aa8a0340ccd82852c
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-08 16:49:01 +0900

    Globalにスタイルを設定した ./theme/theme.ts に内容を書き込んでいる

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
-import "./styles.css";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
     <div className="App">
       {/* ChakraUIを利用する場合には以下のように <ChakraProvider> で囲む必要がある */}
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         {/* Buttonって書くだけでそれなりのボタンがもうきれいにできてる！ｓ */}
         <Button colorScheme="teal"> ボタン </Button>
+        <h1> くんたろ </h1>
       </ChakraProvider>
     </div>
   );
