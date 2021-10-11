f512e87ec52bd5d9a8d97755a970e9540f73d25d
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-11 16:38:34 +0900

    Wrap componentを利用してカードアイテム的なものを表示してみる ChackraUIの中の"Wrap"コンポーネントを利用してユーザのカード配置みたいな機能を実装しているところ。

--- src/components/pages/UserMnagement.tsx
+++ src/components/pages/UserMnagement.tsx
@@ -1,9 +1,23 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <>
-      <p> ユーザ管理画面です </p>
+      <Wrap>
+        {/* 仮の配列を作ってレンダリングを進めてみる */}
+        {[...Array(10)].map(() => (
+          <WrapItem>
+            <div
+              style={{
+                width: "100px",
+                height: "100px",
+                backgroundColor: "green"
+              }}
+            ></div>
+          </WrapItem>
+        ))}
+      </Wrap>
     </>
   );
 });
