75836a4975b41b2b39a116da86befc6d429b7b42
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-12 08:43:16 +0900

    useAllUsersというカスタムフックを作ろう２ 骨格に肉付けをしてコンポーネントが完成した

--- src/hooks/useAllUsers.ts
+++ src/hooks/useAllUsers.ts
@@ -2,10 +2,33 @@
 // カスタムフック＝関数
 // 返却するのは関数と変数
 
-export const useAllUsers = () => {
-  const [loading, setLoading] = useState();
-  const [users, setUsers] = useState();
+import axios from "axios";
+import { useCallback, useState } from "react";
 
-  const getUsers = () => {};
+import { User } from "../types/api/user.ts";
+// メッセージを見せるときにおしゃれに表示するコンポーネントを実装したのでそれを使っていくスタイル
+import { useMessage } from "./useMessage";
+
+export const useAllUsers = () => {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState<Array<User>>();
+  // 手の混んだメッセージ表示のコンポーネント
+  const { showMessage } = userMessage();
+
+  // 他のコンポーネントで　使うときに「再生成されないように」する
+  const getUsers = useCallback(() => {
+    setLoading(true);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch(() => {
+        showMessage({ title: "駄目だよこれは", status: "error" });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
   return { getUsers, loading, users };
 };
