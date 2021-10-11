e83a1694955f25f54312cd5103fdcdefa15c3b67
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-11 16:55:26 +0900

    a bit debugged.

--- src/components/pages/UserMnagement.tsx
+++ src/components/pages/UserMnagement.tsx
@@ -4,8 +4,11 @@ import { memo, VFC } from "react";
 export const UserManagement: VFC = memo(() => {
   return (
     <>
-      <Wrap>
+      {/* chakra UI Wrapを利用すると画面の大きさに応じてコンポーネントサイズを変更してくれる箱を準備してくれる */}
+      {/* これは相当便利やな */}
+      <Wrap spacing="30px">
         {/* 仮の配列を作ってレンダリングを進めてみる */}
+        {/* jak氏がよく使う方法→１０この配列を作成して表示する */}
         {[...Array(10)].map(() => (
           <WrapItem>
             <div
