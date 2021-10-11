0492c564d0fd2d9d3d8c2c56b8bc1747cc55bd7b
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-11 15:59:07 +0900

    useMessage.ts をコードした まだ使ってないよ

---
+++ src/hooks/useMessage.ts
@@ -0,0 +1,26 @@
+import { useToast } from "@chakra-ui/react";
+import { useCallback } from "react";
+
+type Props = {
+  title: string;
+  // 状態については以下の４つしか値を持つことができないという定義の方法
+  status: "info" | "warning" | "success" | "error";
+};
+
+export const useMessage = () => {
+  const toast = useToast();
+  // ちょっとやそっとでは再生成されないようにuseCallbackを指定しておく
+  const showMessage = useCallback((props: Props) => {
+    const { title, status } = props;
+    toast({
+      // 同じ名称のときはコロンを省略して設定することができる
+      title,
+      status;
+      position: "top",
+      duration: 2000,
+      isClosable: true
+    });
+  }, [toast]);
+
+  return { showMessage };
+};
