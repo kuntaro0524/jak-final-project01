db102b5bc602b6256a83fe77fe85b03db4077a95
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-11 10:51:17 +0900

    ヘッダのリンク先を設定した（history.push())を利用

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,23 +1,25 @@
-import { HamburgerIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerOverlay,
-  Flex,
-  Heading,
-  IconButton,
-  Link,
-  useDisclosure
-} from "@chakra-ui/react";
-import { memo, VFC } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
+import { memo, useCallback, VFC } from "react";
+import { useHistory } from "react-router";
 import { MenuIconButton } from "../../atoms/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   // chakra-uiが準備してくれている drawer用のHooks
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // 画面遷移のときの関数を定義しておく
+  const history = useHistory();
+  // 画面遷移はhistory.pushで行える！
+  // 不要な再レンダリングが走らないようにuseCallback()でラップしておく
+  const onClickHome = useCallback(() => history.push("/home"), []);
+  const onClickUserManagement = useCallback(
+    () => history.push("/home/user_management"),
+    []
+  );
+  const onClickSetting = useCallback(() => history.push("/home/setting"), []);
+
   return (
     <>
       {/* // chakra UIのコンポーネントを使って簡単にきれいに見た目を実装 */}
@@ -34,7 +36,13 @@ export const Header: VFC = memo(() => {
       パディングを右側の8px: mr={8}
       ホバーしたときの設定： _hover
       フォントサイズもブレイクポイントごとに切り替えが可能→fontSizeの設定  */}
-        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Flex
+          align="center"
+          as="a"
+          mr={8}
+          _hover={{ cursor: "pointer" }}
+          onClick={onClickHome}
+        >
           <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
             ユーザ管理アプリ
           </Heading>
@@ -48,9 +56,9 @@ export const Header: VFC = memo(() => {
           display={{ base: "none", md: "flex" }}
         >
           <Box pr={4}>
-            <Link>ユーザ一覧</Link>
+            <Link onClick={onClickUserManagement}>ユーザ一覧</Link>
           </Box>
-          <Link> 設定 </Link>
+          <Link onClick={onClickSetting}> 設定 </Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
