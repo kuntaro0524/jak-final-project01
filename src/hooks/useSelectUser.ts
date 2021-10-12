1c93b7ddb99e18d805338e62e5c05e573bd2787e
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-12 12:32:52 +0900

    ユーザを選択して情報を取得・表示する２ カスタムフックを作成した。 ユーザのアレイを useSelectUser に渡す。 onSelectUser 関数はIDが渡されたものと一致するユーザを selectedUser として固定してくれる。

--- src/hooks/useSelectUser.ts
+++ src/hooks/useSelectUser.ts
@@ -1,8 +1,24 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import { User } from "../types/api/user";
+
+// propsの型定義をしっかりとしておくこと
+type Props = {
+  id: number;
+  users: Array<User>;
+};
 
 export const useSelectUser = () => {
-  const [selectedUser, setSelectedUser] = useState();
+  // selectedUserは型としてUserを持っている
+  // useStateの引数の方としてはUserまたはnull ということらしい→よくわからないが
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
 
-  const onSelectUser = useCallback(() => {}, []);
-  return { onSelectUser };
+  const onSelectUser = useCallback((props: Props) => {
+    // propsで親コンポーネントから受け取った値を格納
+    const { id, users } = props;
+    // その情報から一致するユーザを見つける
+    const targetUser = users.find((user) => user.id === id);
+    // 検索で一致したユーザ情報をselected userにセットする
+    setSelectedUser(targetUser);
+  }, []);
+  return { onSelectUser, selectedUser };
 };
