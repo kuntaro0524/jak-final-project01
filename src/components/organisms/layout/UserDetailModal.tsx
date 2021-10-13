5820ed2247abc049349d25e9a31e8159a02d139f
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-13 15:48:48 +0900

    完成版 Modalで出てくる情報をadminであれば（ID=10）編集ができるというModalになった。 完成。 長かったし、何ならよくわからないのでフルスクラッチでこれくらいは書けるようになりたい。

--- src/components/organisms/layout/UserDetailModal.tsx
+++ src/components/organisms/layout/UserDetailModal.tsx
@@ -12,7 +12,7 @@ import {
   Stack,
   ModalFooter
 } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useEffect, useState, VFC } from "react";
 
 // ユーザの情報をインポートしておく
 import { User } from "../../../types/api/user";
@@ -34,7 +34,32 @@ export const UserDetailModal: VFC<Props> = memo((props) => {
   // 親コンポーネントからしっかりuser情報ももらっていく
   const { isOpen, onClose, user, isAdmin } = props;
 
+  // Modalで表示されている情報を編集して更新できるような感じにするために編集に関する情報を state で保持する
+  const [username, setUserName] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  // 表示するときに最初のModalに表示する値を入れとく
+  useEffect(() => {
+    // undefinedの可能性があるので ?? で設定されていなかったら空文字列を設定する
+    setUserName(user?.username ?? "");
+    setName(user?.name ?? "");
+    setEmail(user?.email ?? "");
+    setPhone(user?.phone ?? "");
+    // userが更新されるたびに実行されたいので依存配列にuserを渡しておいてあげる
+  }, [user]);
+
   const onClickUpdate = () => alert("unko shita.");
+  const onChangeUserName = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserName(e.target.value);
+  const onChangeName = (e: ChangeEvent<HTMLInputElement>) =>
+    setName(e.target.value);
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+  const onChangePhone = (e: ChangeEvent<HTMLInputElement>) =>
+    setPhone(e.target.value);
+
   return (
     // 前回UserManagement.tsxで試験した部分をまるっともってきてコンポーネント化している
     // Modalコンポーネントを実装する
@@ -56,13 +81,30 @@ export const UserDetailModal: VFC<Props> = memo((props) => {
               {/* userの情報をちゃんと渡して Modal を表示するようにする */}
               <FormLabel> 名前 </FormLabel>
               {/* isAdminに応じて編集が可能かどうかを変えていく */}
-              <Input value={user?.username} isReadOnly={!isAdmin} />
+              {/* 値が変化したらどうするかということも定義しておく */}
+              <Input
+                value={username}
+                isReadOnly={!isAdmin}
+                onChange={onChangeUserName}
+              />
               <FormLabel> フルネーム </FormLabel>
-              <Input value={user?.name} isReadOnly={!isAdmin} />
+              <Input
+                value={name}
+                isReadOnly={!isAdmin}
+                onChange={onChangeName}
+              />
               <FormLabel> EMAIL </FormLabel>
-              <Input value={user?.email} isReadOnly={!isAdmin} />
+              <Input
+                value={email}
+                isReadOnly={!isAdmin}
+                onChange={onChangeEmail}
+              />
               <FormLabel> TEL </FormLabel>
-              <Input value={user?.phone} isReadOnly={!isAdmin} />
+              <Input
+                value={phone}
+                isReadOnly={!isAdmin}
+                onChange={onChangePhone}
+              />
             </FormControl>
           </Stack>
         </ModalBody>
